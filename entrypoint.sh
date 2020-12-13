c558a838ec2e20f48bf9c6901e5272f1d5a8d935
bo-sfl <bli@sflscientific.com>
2020-12-12 22:46:05 -0800

    Update action output pass

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 echo "Activate Github in last $2 days."
 echo "The orgnization name is $3."
 python /src/main.py --token $1 --last_active $2 --org_name $3
-echo "::set-output name=repo_list::$(cat /src/repos.txt)"
+echo "::set-output name=repo_list::$(cat repos.txt)"
