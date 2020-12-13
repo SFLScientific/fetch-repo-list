3fa0a346a202df5d33c3626fd8ad13f0048a14c3
bo-sfl <bli@sflscientific.com>
2020-12-12 22:33:52 -0800

    Fix action def

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -l
 
 echo "Activate Github in last $2 days."
-echo "The template getting checked is $3."
-echo "The orgnization name is $4."
-repo_list=$(python /src/main.py --token $1 last_active $2 --template_name $3 --org_name $4)
+echo "The orgnization name is $3."
+repo_list=$(python /src/main.py --token $1 last_active $2 --org_name $3)
 echo "::set-output name=repo_list::$repo_list"
