7de816a8bed64c8804438c24412117e5781d761a
bo-sfl <bli@sflscientific.com>
2020-12-12 22:39:48 -0800

    Fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 echo "Activate Github in last $2 days."
 echo "The orgnization name is $3."
-repo_list=$(python /src/main.py --token $1 last_active $2 --org_name $3)
+repo_list=$(python /src/main.py --token $1 --last_active $2 --org_name $3)
 echo "::set-output name=repo_list::$repo_list"
